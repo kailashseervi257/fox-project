9a59ad168849eeabd6226bd7145215c5753409d7
Kailash R <kseervi257@gmail.com>
2020-07-23 16:07:00 +0530

    added local support

--- fox/fox/settings.py
+++ fox/fox/settings.py
@@ -143,3 +143,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
